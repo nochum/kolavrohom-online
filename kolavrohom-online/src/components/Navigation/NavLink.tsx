245d254f6c9643aaa5ad9ef20db22c065c71d4f2
Nochum Klein <nochum@gmail.com>
2025-06-30 05:07:29 -0400

    fixed NavLink template

--- kolavrohom-online/src/components/Navigation/NavLink.tsx
+++ kolavrohom-online/src/components/Navigation/NavLink.tsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 interface NavLinkProps {
   to: string;
   children: React.ReactNode;
+  className?: string;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ to, children }) => {
-  return <Link to={to}>{children}</Link>;
+const NavLink: React.FC<NavLinkProps> = ({ to, children, className }) => {
+  return <Link to={to} className={className}>{children}</Link>;
 };
 
 export default NavLink; 
